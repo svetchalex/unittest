a9995c4b9d6b8f740df9e0cde8f3b3f27e7f299c
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-04-04 09:18:37 +0300

    UnitTests

--- tests/RandomTest.php
+++ tests/RandomTest.php
@@ -8,20 +8,20 @@ class RandomTest extends TestCase
 
     public function testGetNext()
     {
-        $seq = new Random(100);
+        $seq = new Random(7);
         $result = [];
         for ($i = 0; $i < 8; $i++) {
             $result[$i] = $seq->getNext();
         }
-        $expected = [7, 6, 9, 0, 7, 6, 9, 0];
+        $expected = [6, 9, 0, 7, 6, 9, 0, 7];
         $this->assertEquals($expected, $result);
     }
 
     public function testReset()
     {
-        $seq = new Random(100);
+        $seq = new Random(7);
         $seq->reset();
         $result = $seq->getNext();
-        $this->assertEquals(7, $result);
+        $this->assertEquals(6, $result);
     }
 }
